8dfd2d84d46bcb590a1fe4ce7e8d9abadff56e16
Jarrad Merrick <merrickj@live.com.au>
2017-03-21 21:51:54 +0800

    Bug fixes: Incorrect object setup notation (commas, not semicolons) Fixed tooltip width not snapping back upon resetting tooltip with shorter text.

--- tooltip.js
+++ tooltip.js
@@ -1,26 +1,25 @@
 var tooltip = {
-    'ttPointWidth' : 20;
-    'ttPointHeight' : 21;
-    'ttPointMid' : 11;
-    'ResetToolTip' : function(txt) {
-            $('#ttText').html("This is the tooltip<br>It has 3 lines<br>To it");
-            //$('#ttText').hide();
-            $('#ttText').css({left: "left top", at: "left bottom", of: "#ttRight"});
-            //alert($('#ttText').height());
-            
-            
+    ttPointWidth : 20,
+    ttPointHeight : 21,
+    ttPointMid : 11,
+    ResetToolTip : function(txt) {
+            // sets this back to initial default width each time we change text
+            // otherwise width will stay as the greatest width is has been, ie.
+            // won't snap back to a smaller width.
+            $('.ttRight').css({width: 'initial'});
             $('.ttText').html(txt);
             $('.ttContainer').show();
+
             var ttWidth =  Math.ceil($('.ttText').width());
             var ttHeight = Math.ceil($('.ttText').height());
             $('.ttContainer').hide();
-            $('.ttRight').css({width: this.ttWidth, height: this.ttHeight});
+            $('.ttRight').css({width: ttWidth, height: ttHeight});
             // ttBorderLeft.height = ttHeight + ttRight.padding(10px) + ttRight.border(2px) - ttPoint.height(21px) = ttHeight - 9
-            $('.ttBorderLeft').css({height: this.ttHeight - 9});
+            $('.ttBorderLeft').css({height: ttHeight - 9});
             $('.ttPoint').html("<img src='tooltippoint.png'>");
             $('.ttContainer').css({top: '500px', left: '500px'}); 
-        }
-    'InitToolTip' : function() {
+        },
+    InitToolTip : function() {
             // tooltip div elements
             $('body').append("<div class='ttContainer'>" + 
                 "<div class='ttleft'>" +
