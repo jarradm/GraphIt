b7599ce7b9ed1fb1e89c3b105aab06854b823b6c
Jarrad Merrick <merrickj@live.com.au>
2017-03-31 17:46:33 +0800

    Refactord graphing code, into an object.

--- graphit.js
+++ graphit.js
@@ -1,52 +1,38 @@
-var count = 1;
-
-// Returns angle between two points in radians
-function GetAngleBetweenPoints(p1, p2)
-{
-    var length = p2.x - p1.x
-    var height = p2.y - p1.y
+var graphIt = {
+    lineCount : 1,
+    GetAngleBetweenPoints : function(p1, p2) {
+        var length = p2.x - p1.x
+        var height = p2.y - p1.y
     
-    // vertical line
-    if(length == 0)
-    {
-        return Math.PI / 2;
+        // vertical line
+        if(length == 0) { return Math.PI / 2; }
+        // horizontal line
+        if(height == 0) { return 0; }
+    
+        // angle in radians
+        return Math.atan(height / length);
+    },
+    GetDistanceBetweenPoints : function(p1, p2) {
+        var length = p2.x - p1.x;
+        var height = p2.y - p1.y;
+        
+        return Math.sqrt(length*length + height*height);
+    },
+    DrawLine : function(p1, p2, el) {
+        var width = this.GetDistanceBetweenPoints(p1, p2);
+        var angle = this.GetAngleBetweenPoints(p1, p2);
+    
+        $(el).append("<div class='line' id='line" + (this.lineCount++) + "'></div>");
+        $('#line' + (this.lineCount-1)).css({"webkitTransform":"rotate(" + angle + "rad)", "left":p1.x, 
+            "top":p1.y, "width":width, "height":1});
+    },
+    DrawLinesBetweenPoints : function(p, el) {
+        for(i=0; i<p.length-1; i++)
+        {
+            this.DrawLine(p[i], p[i+1], el);
+        }
     }
-    
-    // horizontal line
-    if(height == 0)
-    {
-        return 0;
-    }
-    
-    return Math.atan(height / length);
-}
-
-// Returns disatance between two points
-function GetDistanceBetweenPoints(p1, p2)
-{
-    var length = p2.x - p1.x;
-    var height = p2.y - p1.y;
-    
-    return Math.sqrt(length*length + height*height);
-}
-
-// Draws a straight line between two points
-function DrawLine(p1, p2, container)
-{
-    width = GetDistanceBetweenPoints(p1, p2);
-    angle = GetAngleBetweenPoints(p1, p2);
-    
-    $(container).append("<div class='line' id='line" + (count++) + "'></div>");
-    $('#line' + (count-1)).css({"webkitTransform":"rotate(" + angle + "rad)", "left":p1.x, "top":p1.y, "width":width, "height":1});
-}
-
-function DrawLinesBetweenPoints(p, container)
-{
-    for(i=0; i<p.length-1; i++)
-    {
-        DrawLine(p[i], p[i+1], container);
-    }
-}
+};
 
 /*
 var p1 = {x:10, y:200};
@@ -70,4 +56,4 @@ var p5 = {x:370, y:250};
 var p6 = {x:460, y:200};
 
 p = [p1, p2, p3, p4, p5, p6];
-DrawLinesBetweenPoints(p, ".container");
+graphIt.DrawLinesBetweenPoints(p, ".container");
